3262e7c633051abd47bdd8108bbaffbfa53e5b93
diego-ronan <diegoronanfelix@gmail.com>
2024-01-28 16:54:26 -0300

    API, Models and Migrations

--- database/migrations/2024_01_28_104803_create_courses_table.php
+++ database/migrations/2024_01_28_104803_create_courses_table.php
@@ -11,11 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->id();
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('courses', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->text('description');
+            $table->string('image')->nullable();
+            $table->timestamps();
         });
     }
 
@@ -24,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('courses');
     }
 };
